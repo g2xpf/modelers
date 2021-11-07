1ed8b437ad818b7433ef7961a8d980196f9e6a06
g2xpf <nameko.jack@gmail.com>
2021-11-08 01:43:00 +0900

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
-use winit::event::VirtualKeyCode;
-use winit::event_loop::ControlFlow;
-fn main() {
-    let event_loop = winit::event_loop::EventLoop::new();
-    let window = winit::window::Window::new(&event_loop);
+use winit::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::Window;
 
-    event_loop.run(|event, _, control_flow| match event {
-        winit::event::Event::WindowEvent { event, .. } => match event {
-            winit::event::WindowEvent::KeyboardInput {
-                input:
-                    winit::event::KeyboardInput {
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    },
-                ..
-            } => {
+fn main() {
+    let event_loop = EventLoop::new();
+    let _window = Window::new(&event_loop);
+
+    event_loop.run(|event, _, control_flow| {
+        if let Event::WindowEvent {
+            event:
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            state: ElementState::Pressed,
+                            ..
+                        },
+                    ..
+                },
+            ..
+        } = event
+        {
+            {
                 *control_flow = ControlFlow::Exit;
             }
-            _ => {}
-        },
-        _ => {}
+        }
     });
 }
